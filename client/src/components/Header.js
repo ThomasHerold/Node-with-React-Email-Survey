01d835c3f79968c7c044671ed0b48b1f6217307b
Thomas Herold <thomas.herold95@gmail.com>
2020-04-21 12:30:05 -0400

    Fixed header logo route

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
           <nav>
             <div className="nav-wrapper">
               <Link 
-                to={ this.props.auth ? "/surveys'" : "/" } 
+                to={ this.props.auth ? "/surveys" : "/" } 
                 className="left brand-logo" 
                 style={{ marginLeft: 10 }} 
               >
